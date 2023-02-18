223d512449d1193860d61ac4cfefb905bbac14e0
xTrainx <daniel_b-30@hotmail.com>
2023-02-18 01:23:11 -0600

    how do i reach

--- tgui/packages/tgui/interfaces/Jukebox.js
+++ tgui/packages/tgui/interfaces/Jukebox.js
@@ -37,12 +37,13 @@ export const Jukebox = (props, context) => {
               content={active ? 'Stop' : 'Play'}
               selected={active}
               onClick={() => act('toggle')} />
-            <Button
+             <Button
               icon={active ? 'pause' : 'play'}
               content={active ? 'Eject' : 'Eject'}
               selected={active}
-              onClick={() => act('eject_disk')} /></>
-            )}>
+              onClick={() => act('eject_disk')} />
+            </>
+         )}>
           <LabeledList>
             <LabeledList.Item label="Track Selected">
               <Dropdown
