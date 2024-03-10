314d3dd740424b269325b943086397f6ab276da9
ConstantineII <138142837+ConstantineII@users.noreply.github.com>
2024-03-10 19:30:36 -0400

    Fixes the game

--- tools/ci/install_auxmos.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-source dependencies.sh
-
-mkdir -p ~/.byond/bin
-wget -O ~/.byond/bin/libauxmos.so "https://github.com/sunset-wasteland/auxmos/releases/download/v2.3.0-preview-01-07-2023/libauxmos.so"
-chmod +x ~/.byond/bin/libauxmos.so
-ldd ~/.byond/bin/libauxmos.so
